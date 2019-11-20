6387b1a0d3bb6fae1e72dd0aa0d68a87a01ad6ea
Katrina Fuller <trinafuller@att.net>
2019-11-20 16:56:53 -0700

    adding authService

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Axios from 'axios'
+import AuthService from '../../AuthService'
+import router from "../router/index"
 
 Vue.use(Vuex)
 
@@ -18,11 +20,45 @@ export default new Vuex.Store({
     challenges: []
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user
+    },
+    resetState(state) {
+      state.user = []
+    },
     setChallenges(state, challenges) {
       state.challenges = challenges
     }
   },
   actions: {
+    async register({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Register(creds)
+        commit('setUser', user)
+        router.push({ name: "home" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async login({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Login(creds)
+        commit('setUser', user)
+        router.push({ name: "home" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async logout({ commit, dispatch }) {
+      try {
+        let success = await AuthService.Logout()
+        if (!success) { }
+        commit('resetState')
+        router.push({ name: "login" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
     getChallenges({ commit }) {
       api.get('challenges')
         .then(res => {
