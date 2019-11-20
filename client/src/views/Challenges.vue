dd203c2b398609cac6e2b964f411816a1f066584
Katrina Fuller <trinafuller@att.net>
2019-11-19 17:10:54 -0700

    creating route for challenges

---
+++ client/src/views/Challenges.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="challenges">hi</div>
+</template>
+
+
+<script>
+export default {
+  name: "challenges",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
