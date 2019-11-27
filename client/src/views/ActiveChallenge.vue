f272f2881439952c9eadae8a755616f24e03e6fd
Katrina Fuller <trinafuller@att.net>
2019-11-26 18:06:53 -0700

    starting activeChallenge view

---
+++ client/src/views/ActiveChallenge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="activehallenge"></div>
+</template>
+
+
+<script>
+export default {
+  name: "activehallenge",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
